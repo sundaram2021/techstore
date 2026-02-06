ef17b7ec4c75b204ba850dab3ba64a63df67792c
CharlieHelps <charlie@charlielabs.ai>
2026-02-06 20:20:16 +0000

    fix(tambo): render icon reliably in prod

--- src/components/tambo/fab-button.tsx
+++ src/components/tambo/fab-button.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import { motion } from "framer-motion";
+import Image from "next/image";
 import { fabVariants, fabIconVariants, pulseKeyframes, pulseTransition } from "./animations";
+import tamboPng from "../../../public/tambo.png";
 
 interface FabButtonProps {
     isOpen: boolean;
@@ -40,11 +42,13 @@ export function FabButton({ isOpen, onClick }: FabButtonProps) {
                 transition={{ duration: 0.3 }}
                 className="relative z-10 w-9 h-9 sm:w-10 sm:h-10 flex items-center justify-center"
             >
-                {/* eslint-disable-next-line @next/next/no-img-element */}
-                <img
-                    src="/tambo.png"
+                <Image
+                    src={tamboPng}
                     alt="ShopMate AI"
+                    width={40}
+                    height={40}
                     className="w-full h-full object-contain drop-shadow-md"
+                    priority
                 />
             </motion.div>
 
